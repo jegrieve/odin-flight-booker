1cc9452d3b2713dd4b9ee85b9acccc6e06a22dcc
James Grieve <pocojames@hotmail.com>
2021-02-27 02:26:28 -0800

    add airport model, and corresponding db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Airport.delete_all
+airports = ["JNU","FLG","PHX","NYC","SFO"]
+
+airports.each do |airport|
+Airport.create(code: airport)
+end
