daacbb0da797c2f68a95a665c4ef8bbd2bd2115a
James Grieve <pocojames@hotmail.com>
2021-02-27 23:52:24 -0800

    add flight db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Airport.delete_all
-airports = ["JNU","FLG","PHX","NYC","SFO"]
+# Airport.delete_all
+
+airports = ["NYC","SFO"]
 
 airports.each do |airport|
 Airport.create(code: airport)
 end
+
+flights = Flight.create([
+  { from_airport_id: airports.first.id, to_airport_id: airports.last.id, duration: 320, scheduled_on: "2021-10-21 07:00:00"},
+  { from_airport_id: airports.first.id, to_airport_id: airports.last.id, duration: 320, scheduled_on: "2021-10-22 08:00:00"},
+  { from_airport_id: airports.first.id, to_airport_id: airports.last.id, duration: 320, scheduled_on: "2021-10-23 09:00:00"},
+  { from_airport_id: airports.last.id, to_airport_id: airports.first.id, duration: 385, scheduled_on: "2021-10-24 10:00:00"},
+  { from_airport_id: airports.last.id, to_airport_id: airports.first.id, duration: 385, scheduled_on: "2021-10-25 11:00:00"},
+  { from_airport_id: airports.last.id, to_airport_id: airports.first.id, duration: 385, scheduled_on: "2021-10-26 12:00:00"}
+])
+
+
