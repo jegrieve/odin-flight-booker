5b8f56ac7f58abdd4db1d30ee515da9092e17524
James Grieve <pocojames@hotmail.com>
2021-02-28 15:10:18 -0800

    add search form for flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,11 @@
 class FlightsController < ApplicationController
     def index
         @flights = Flight.all
+        @airport_options = Airport.all.map {|airport| [airport.code, airport.id]}
+        @passenger_options = [['1',1],['2',2],['3',3],['4',4]]
+        @year_options = Flight.all.map { |flight| flight.scheduled_on.year }.uniq
+        @month_options = Flight.all.map { |flight| flight.scheduled_on.month }.uniq
+        @day_options = Flight.all.map { |flight| flight.scheduled_on.day }.uniq
       end
     
       def new
