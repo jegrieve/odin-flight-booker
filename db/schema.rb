5c74f891cb3aa56ec8e4217dd00d88da2c08adab
James Grieve <pocojames@hotmail.com>
2021-03-01 21:19:45 -0800

    add associations between bookings, passengers and flights

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_232819) do
+ActiveRecord::Schema.define(version: 2021_03_02_051718) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_03_01_232819) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "flight_id"
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -34,6 +36,17 @@ ActiveRecord::Schema.define(version: 2021_03_01_232819) do
     t.index ["to_airport_id"], name: "index_flights_on_to_airport_id"
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "booking_id"
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
+  end
+
+  add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "from_airport_id"
   add_foreign_key "flights", "airports", column: "to_airport_id"
+  add_foreign_key "passengers", "bookings"
 end
