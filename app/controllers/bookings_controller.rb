e2932db93df7a61233d9fad22c998a46a002d769
James Grieve <pocojames@hotmail.com>
2021-03-02 01:17:17 -0800

    fix nested passenger forms

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,11 @@
 class BookingsController < ApplicationController
+    def new
+        @booking = Booking.new
+        @selected_flight = Flight.find(params[:flight_info])
+        @passengers_count = params[:num_passengers].to_i
+        @passengers_count.times { @booking.passengers.build }
+    end
+
+    def create
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root "flights#index"
   resources :airports
   resources :flights
+  resources :bookings
 end
