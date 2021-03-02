1bca478395fa6e387dc4addc593e52b401d994b3
James Grieve <pocojames@hotmail.com>
2021-03-02 14:33:55 -0800

    app complete

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,23 @@
 class BookingsController < ApplicationController
     def new
         @booking = Booking.new
-        @selected_flight = Flight.find(params[:flight_info])
+        @selected_flight = Flight.find(params[:flight_id])
         @passengers_count = params[:num_passengers].to_i
         @passengers_count.times { @booking.passengers.build }
     end
 
     def create
-    end
+        @booking = Booking.create(booking_params)
+          redirect_to booking_path(@booking)
+      end
+
+      def show
+        @booking = Booking.find(params[:id])
+      end
+
+    private
+
+    def booking_params
+        params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
+      end
 end
